53967b77bfca9c114e1ef22e52b6e6ddfbae8a0e
Heather Christman <efl7a@yahoo.de>
2018-09-04 19:32:43 -0400

    still working on POSTing new comments

--- client/src/actions/commentActions.js
+++ client/src/actions/commentActions.js
@@ -12,11 +12,13 @@ export function addComment (content, petitionId) {
 
   return (dispatch) => {
     dispatch({ type: "ADDING_COMMENTS", payload: comment });
-    return fetch('api/comments', {
+    console.log("this is comment", comment)
+    const response = fetch('/api/comments', {
       method: 'post',
       headers: { "Content-Type": "application/json" },
       data: { comment: { content: content, petition_id: petitionId } }
-    }).then(response => response.json())
-        .then(comments => dispatch({ type: 'FETCH_COMMENTS', payload: comments}))
+      })
+      .then(response => response.json())
+      .then(comments => dispatch({ type: 'FETCH_COMMENTS', payload: comments}))
   }
 }
